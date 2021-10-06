45cbe110f0204d768e092bcefa0157b5803f48da
田辺　哲也 <tanabetetsuya@tanabetuyanoMBP.elecom>
2021-10-06 16:37:01 +0900

    [Add]関数スコープ、グローバルスコープの追加

--- sample.js
+++ sample.js
@@ -2,3 +2,12 @@ var nickname = "taro"
 console.log(nickname);
 var nickname = "ichiro"
 console.log(nickname);
+
+var str = "webcamp" //グローバルスコープ
+
+fanction foo(){
+  console.log(str);
+  var y = "hello" //関数スコープ
+}
+
+console.log(y);
